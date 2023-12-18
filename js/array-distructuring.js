0238ee174e9253c590ac811af0b101c6c1935c24
mirza-azam <mirzaazam884@gmail.com>
2023-12-18 23:04:34 +0600

    object distructuring practice

--- js/array-distructuring.js
+++ js/array-distructuring.js
@@ -2,3 +2,20 @@
 const arr = [20, 56, 67, 43, 23];
 const  [first, second, third] = arr;
 console.log (first, second, third);
+
+
+// Object distructiong
+const object = {
+    name: 'leptop',
+    brand: 'hp',
+    price: 54000,
+    color: 'silver',
+    quality: {
+        processor: 'intel',
+        core: 'i5',
+        gen: '8th'
+    }
+}
+
+const {processor, core, gen} = object.quality
+console.log(processor, core, gen);
